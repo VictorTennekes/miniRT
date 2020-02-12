6b2b9a46a88556b80b423d02893a7271abbdadb8
Victor Tennekes <victor@tennekes.nl>
2020-02-12 15:48:01 +0100

    Feat: added obj_sphere_dist

--- src/dist/obj_dist_sp.c
+++ src/dist/obj_dist_sp.c
@@ -14,5 +14,23 @@
 
 t_ray_res	obj_dist_sp(t_object *sphere, t_ray ray)
 {
-	
+	double	d;
+	double	t;
+	double	h;
+	double tmp;
+	t_vec3d	l;
+	t_vec3d	p;
+
+	l = vec_sub(sphere->pos, ray.origin);
+	t = vec_dot_prod(l, ray.direction);
+	if (t < 0)
+		return (ray_res_inf());
+	tmp = vec_len(l);
+	d = sqrt(pow(tmp, 2) - pow(t, 2));
+	if (d < 0)
+		return (ray_res_inf());
+	if (d < (sphere->size / 2))
+		return (ray_res_inf());
+	h = sqrt(pow(sphere->size / 2, 2) - pow(d, 2));
+	return (ray_res_dist_new(sphere, /*stuff*/, sphere->color, t - h);
 }
