30259eeb3d24085dbd6d08bbc2a9538af19d3ac9
Victor Tennekes <victor@tennekes.nl>
2020-02-25 17:09:15 +0100

    Feat: resolved #3

--- src/vec/get_pixel.c
+++ src/vec/get_pixel.c
@@ -30,7 +30,7 @@ t_vec3d		look_at(t_camera  *camera, t_vec3d ray)
 	t_vec3d	right;
 	t_vec3d up;
 
-	forward = camera->vector;
+	forward = vec_multi(camera->vector, -1);
 	right = vec_cross_prod(vec_normalize(vec_new(0, 1, 0)), forward);
 	up = vec_cross_prod(forward, right);
 	t_matrix matrix;
@@ -50,14 +50,13 @@ t_color		get_pixel(t_vec2ui pixel, t_data *data)
 	fov_fact = tan(camera->fov / 2 * (M_PI / 180));
 	ray.origin.x = (2 * ((pixel.x + 0.5) / WINDOW.x) - 1) * fov_fact;
 	ray.origin.y = (1 - (2 * ((pixel.y + 0.5) / WINDOW.y))) * fov_fact;
-	ray.origin.z = 1;
+	ray.origin.z = -1;
 	ray.origin = look_at(camera, ray.origin);
-	ray.origin = vec_add(ray.origin, camera->pos);
 	if (WINDOW.x > WINDOW.y)
 		ray.origin.x *= WINDOW.x / (double)WINDOW.y;
 	else
 		ray.origin.y *= WINDOW.y / (double)WINDOW.x;
-	ray.direction = vec_a_to_b(camera->pos, ray.origin);
+	ray.direction = vec_normalize(ray.origin);
 	// printf("RAY DIRECTION\nx: %f\ny: %f\nz: %f\n", ray.direction.x, ray.direction.y, ray.direction.z);
 	return(cast_ray(ray, data));
 } 
