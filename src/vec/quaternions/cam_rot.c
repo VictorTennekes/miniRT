0c735a86f6a5b76ed3b330d82cc36438ce6760e8
Victor Tennekes <victor@tennekes.nl>
2020-03-06 15:47:56 +0100

    Refactor: merge up to Norme

--- src/vec/quaternions/cam_rot.c
+++ src/vec/quaternions/cam_rot.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   cam_rot.c                                          :+:      :+:    :+:   */
+/*   cam_rot.c                                          :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: vtenneke <vtenneke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/03/04 13:38:54 by vtenneke       #+#    #+#                */
-/*   Updated: 2020/03/06 15:37:37 by aaugusti         ###   ########.fr       */
+/*   Updated: 2020/03/06 15:37:37 by aaugusti      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,14 @@
 
 #include <minirt.h>
 
-#define P0 (p[0])
-#define P1 (p[1])
-#define R0 (r[0])
-#define R1 (r[1])
-
 t_quat	rotate_cam(t_vec3d vector, t_vec3d axis, double angle)
 {
 	t_quat	p[2];
 	t_quat	r[2];
 
-	P0 = quat_new(0, vector.x, vector.y, vector.z);
-	R0 = quat_local_rot(axis, angle);
-	R1 = quat_conj(R0);
-	P1 = quat_multi(quat_multi(R0, P0), R1);
-	return (P1);
+	p[0] = quat_new(0, vector.x, vector.y, vector.z);
+	r[0] = quat_local_rot(axis, angle);
+	r[1] = quat_conj(r[0]);
+	p[1] = quat_multi(quat_multi(r[0], p[0]), r[1]);
+	return (p[1]);
 }
