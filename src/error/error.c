25627df4f1b876936cdd7652e5f07a925a12d90a
Victor Tennekes <victor@tennekes.nl>
2020-04-14 11:52:27 +0200

    Fix: error up to subject norm

--- src/error/error.c
+++ src/error/error.c
@@ -5,7 +5,7 @@
 /*                                                     +:+                    */
 /*   By: vtenneke <vtenneke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2020/01/24 16:56:02 by vtenneke       #+#    #+#                */
+/*   Created: 2020/01/24 16:56:02 by vtenneke      #+#    #+#                 */
 /*   Updated: 2020/02/05 18:46:37 by vtenneke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
@@ -25,7 +25,7 @@
 
 void	print_error(char *error, t_data *data)
 {
-	ft_putstr_fd("Error: ", 1);
+	ft_putstr_fd("Error\n", 1);
 	ft_putstr_fd(error, 1);
 	ft_putstr_fd("\n", 1);
 	if (data)
