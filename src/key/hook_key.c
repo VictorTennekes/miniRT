61620bd5ff44fbcd01e5f0a25465940c7abf2bd0
Victor Tennekes <victor@tennekes.nl>
2020-05-07 16:57:59 +0200

    Chore: removed print for testing

--- src/key/hook_key.c
+++ src/key/hook_key.c
@@ -29,8 +29,6 @@ int		hook_key(int keycode, t_data *data)
 	mlx_string_put(data->mlx_info.mlx, data->mlx_info.mlx_win,
 		(data->window.x / 2) - 10, 20, 0xFFFFFF, ft_itoa(keycode));
 	key(keycode, data);
-	printf("camera->pos: x: %f y: %f z: %f\n", data->current_cam->pos.x, data->current_cam->pos.y, data->current_cam->pos.z);
-	printf("camera_vector->pos: x: %f y: %f z: %f\n", data->current_cam->vector.x, data->current_cam->vector.y, data->current_cam->vector.z);
 	return (0);
 }
 
